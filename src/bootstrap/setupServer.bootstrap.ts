1259928a5b31c66c8f78ab7ab27d0023e09cd054
dev-miguel-resources <ingenieromiguelch@gmail.com>
2026-01-05 22:02:35 -0300

    working in setup server

--- src/bootstrap/setupServer.bootstrap.ts
+++ src/bootstrap/setupServer.bootstrap.ts
@@ -1,1 +1,66 @@
+import { Application, Request, Response, NextFunction, json, urlencoded } from 'express';
+import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
+import Logger from 'bunyan';
+import cookieSession from 'cookie-session';
+import { config } from '@configs/configEnvs';
+import { logger } from '@configs/configLogs';
 
+const log: Logger = logger.createLogger('server');
+
+// SOLID Principle: Single Responsability
+// SOLID Principle: Open/Closed
+export class SocialServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    // inicializaciones de métodos a arrancar cuando se levante la app
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    // Manejo de la seguridad del servidor con owasp
+    // Design Pattern Synchronized Token: https://medium.com/@kaviru.mihisara/synchronizer-token-pattern-e6b23f53518e
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
+        maxAge: 24 * 7 * 3600000, // 1 semana
+        secure: config.NODE_ENV !== 'development'
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: config.CLIENT_URL,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      })
+    );
+  }
+
+  // Definiciones generales
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
+
+  // Manejo de rutas de express
+  private routesMiddleware(app: Application): void {}
+
+  // Manejo de errores globales
+  private globalErrorHandler(app: Application): void {}
+
+  // Configuraciones de arranque
+  private startHttpServer(httpServer: http.Server): void {}
+}
--- src/configs/configEnvs.ts
+++ src/configs/configEnvs.ts
@@ -10,10 +10,18 @@ const log: Logger = logger.createLogger('logEnvs');
 class ConfigEnvs {
   public DATABASE_URL: string | undefined;
   public SERVER_PORT: string | undefined;
+  public CLIENT_URL: string | undefined;
+  public SECRET_KEY_ONE: string | undefined;
+  public SECRET_KEY_TWO: string | undefined;
+  public NODE_ENV: string | undefined;
 
   constructor() {
     this.DATABASE_URL = process.env.DATABASE_URL;
     this.SERVER_PORT = process.env.SERVER_PORT;
+    this.CLIENT_URL = process.env.CLIENT_URL;
+    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE;
+    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO;
+    this.NODE_ENV = process.env.NODE_ENV;
   }
 
   // Método que valida que ninguna variable de entorno a utilizar sea undefined
